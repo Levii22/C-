3e1e72f8c529ff25942d63500c1cb233f9f99014
Levi <dragonball14499@gmail.com>
2023-01-27 18:10:15 +0200

    create connection

--- EMS/departments.cs
+++ EMS/departments.cs
@@ -17,6 +17,7 @@ namespace EMS
         public departments()
         {
             InitializeComponent();
+            Con = new Functions();
         }
 
         private void label2_Click(object sender, EventArgs e)
