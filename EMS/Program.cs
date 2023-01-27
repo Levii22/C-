2c509893c060fd7a7549501c63002755250678ec
Levi <dragonball14499@gmail.com>
2023-01-27 17:15:34 +0200

    Change start page to Login form

--- EMS/Program.cs
+++ EMS/Program.cs
@@ -1,3 +1,5 @@
+using EMS;
+
 namespace ems
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace ems
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new employees());
+            Application.Run(new Login());
         }
     }
 }
