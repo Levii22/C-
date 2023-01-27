4d49eb07f445db6dd042c7d1fb333cba45a5aa5a
Levi <dragonball14499@gmail.com>
2023-01-27 18:10:31 +0200

    create list departments function

--- EMS/departments.cs
+++ EMS/departments.cs
@@ -20,6 +20,9 @@ namespace EMS
             Con = new Functions();
         }
 
+        private void ListDepartments()
+        {
+        }
         private void label2_Click(object sender, EventArgs e)
         {
 
